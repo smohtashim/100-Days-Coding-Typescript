1310488d85c6ae9adcbd0570cafaa00a6dbc6952
Syed Mohtashim <159305613+smohtashim@users.noreply.github.com>
2024-03-10 01:55:51 +0500

    Add files via upload

---
+++ Day 9/Q25_Q26_Q27.js
@@ -0,0 +1,92 @@
+// Q25
+var alien_color = "blue";
+if (alien_color === "blue") {
+    console.log("The Alien's color is Blue.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
+// Q26
+alien_color = "purple";
+// Check the color of the alien using an if-else chain
+if (alien_color === "green") {
+    console.log("The Alien's color is Green.");
+}
+else if (alien_color === "yellow") {
+    console.log("The Alien's color is Yellow.");
+}
+else if (alien_color === "red") {
+    console.log("The Alien's color is Red.");
+}
+else if (alien_color === "purple") {
+    console.log("The Alien's color is Purple.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
+// Q27
+alien_color = "yellow";
+// Check the color of the alien using an if-else chain
+if (alien_color === "green") {
+    console.log("The Alien's color is Green.");
+}
+else if (alien_color === "yellow") {
+    console.log("The Alien's color is Yellow.");
+}
+else if (alien_color === "red") {
+    console.log("The Alien's color is Red.");
+}
+else if (alien_color === "purple") {
+    console.log("The Alien's color is Purple.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
+alien_color = "green";
+if (alien_color === "green") {
+    console.log("The Alien's color is Green.");
+}
+else if (alien_color === "yellow") {
+    console.log("The Alien's color is Yellow.");
+}
+else if (alien_color === "red") {
+    console.log("The Alien's color is Red.");
+}
+else if (alien_color === "purple") {
+    console.log("The Alien's color is Purple.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
+alien_color = "red";
+if (alien_color === "green") {
+    console.log("The Alien's color is Green.");
+}
+else if (alien_color === "yellow") {
+    console.log("The Alien's color is Yellow.");
+}
+else if (alien_color === "red") {
+    console.log("The Alien's color is Red.");
+}
+else if (alien_color === "purple") {
+    console.log("The Alien's color is Purple.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
+alien_color = "black";
+if (alien_color === "green") {
+    console.log("The Alien's color is Green.");
+}
+else if (alien_color === "yellow") {
+    console.log("The Alien's color is Yellow.");
+}
+else if (alien_color === "red") {
+    console.log("The Alien's color is Red.");
+}
+else if (alien_color === "purple") {
+    console.log("The Alien's color is Purple.");
+}
+else {
+    console.log("Unknown Alien color.");
+}
